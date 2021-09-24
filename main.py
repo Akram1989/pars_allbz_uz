116b96e21483f96f7a6a1702cc8d71feb4f2acde
akram.djuraev <akram.djuraev@gmail.com>
2021-09-24 10:57:31 +0500

    .

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs 
 
-# need to add description - отличается от характеристики
 def parse_allbazar_toys():  # парсинг игрушек с сайта allbazar.top и пропись товаров в csv или лучше json
     
     for page in range (1):
         url = f'https://allbazar.uz/cat/api/getcat.php?params%5Bfilter%5D%5BACTIVE%5D=Y&params%5Bfilter%5D%5B!PREVIEW_PICTURE%5D=false&params%5Bfilter%5D%5BINCLUDE_SUBSECTIONS%5D=Y&params%5Bfilter%5D%5BSECTION_ID%5D=103&params%5Bclass%5D=col-md-3%20mb-3%20col-20%20col-6&params%5Bsort%5D%5BSHOW_COUNTER%5D=DESC&params%5Bsort%5D%5BSECTION_ID%5D=RAND&params%5Bpagen%5D%5BnPageSize%5D=10&params%5Bpagen%5D%5BiNumPage%5D={page}'
+        little_url = 'https://allbazar.uz'
         HEADERS = {'user_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"}
         s = requests.session()
         r = requests.get(url, headers=HEADERS)
@@ -30,10 +30,11 @@ def parse_allbazar_toys():  # парсинг игрушек с сайта allbaz
             main_photo = 'https://allbazar.uz' + main_img
             dir = soup.find('div', class_="col-2").find_all('img')
             for i in dir:
-                imgs = url + i['data-big']
+                imgs = little_url + i['data-big']
                 additional_photos.append(imgs)
-            product_name = soup.find('h1', class_='h3 mb-3').get_text()  # DONE
+            product_name = soup.find('h1', class_='h3 mb-3').get_text()
             product_price = soup.find('span', class_='listPrice').get_text().replace('uzs',' сум')  # DONE - NEED TO TRY ADD MARGIN
+            product_description = soup.find('div', class_='container mt-3').find('div', class_='mt-2')
             characteristics = soup.find('div', attrs='br-block p-3 bg-white')
             rows = characteristics.find_all(attrs='col-md-6')
 
@@ -44,9 +45,11 @@ def parse_allbazar_toys():  # парсинг игрушек с сайта allbaz
 
         print(product_name,)
         print(product_price)
+        print(product_description)
         print(characteristics1)
         print( main_photo,)
         print(additional_photos)
+
 
 parse_allbazar_toys()
 
