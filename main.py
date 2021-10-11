4899c6cf7bd715f054fa59bfdb915f02e193e372
akram.djuraev <akram.djuraev@gmail.com>
2021-10-11 20:56:30 +0500

    11.10.2021 - Done

--- main.py
+++ main.py
@@ -2,71 +2,57 @@ import requests
 from bs4 import BeautifulSoup as bs
 import json
 
+#Добавить сверху блок кода, чтобы парсил ссылки с категориями, из них все что вытекает ниже, тем самым весь сайт
 
-product = {}
+product_ulr_list = []
+products = []
+HEADERS = {
+                'user_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"
+        }
 
+count = 0
 
+for page in range(1):
+    url = f'https://allbazar.uz/cat/api/getcat.php?params%5Bfilter%5D%5BACTIVE%5D=Y&params%5Bfilter%5D%5B!PREVIEW_PICTURE%5D=false&params%5Bfilter%5D%5BINCLUDE_SUBSECTIONS%5D=Y&params%5Bfilter%5D%5BSECTION_ID%5D=103&params%5Bclass%5D=col-md-3%20mb-3%20col-20%20col-6&params%5Bsort%5D%5BSHOW_COUNTER%5D=DESC&params%5Bsort%5D%5BSECTION_ID%5D=RAND&params%5Bpagen%5D%5BnPageSize%5D=10&params%5Bpagen%5D%5BiNumPage%5D={page}'
+    r = requests.get(url, headers=HEADERS)
+    soup = bs(r.content, 'lxml')
+    goods = soup.find_all(class_='d-block')
+    for i in goods:
+        prd_raw_link = i.get('href')
+        product_ulr_list.append('https://allbazar.uz' + prd_raw_link)
 
-def parse_allbazar_toys():  # парсинг игрушек с сайта allbazar.top и пропись товаров в csv или лучше json
-    with open('test.json', 'w') as file:
-        for page in range (1):
-            url = f'https://allbazar.uz/cat/api/getcat.php?params%5Bfilter%5D%5BACTIVE%5D=Y&params%5Bfilter%5D%5B!PREVIEW_PICTURE%5D=false&params%5Bfilter%5D%5BINCLUDE_SUBSECTIONS%5D=Y&params%5Bfilter%5D%5BSECTION_ID%5D=103&params%5Bclass%5D=col-md-3%20mb-3%20col-20%20col-6&params%5Bsort%5D%5BSHOW_COUNTER%5D=DESC&params%5Bsort%5D%5BSECTION_ID%5D=RAND&params%5Bpagen%5D%5BnPageSize%5D=10&params%5Bpagen%5D%5BiNumPage%5D={page}'
-            short_url = 'https://allbazar.uz'
-            HEADERS = {
-                'user_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"}
-            s = requests.session()
-            r = requests.get(url, headers=HEADERS)
-            soup = bs(r.content, 'lxml')
-            # print(soup) - checkpoint
-            list_item = soup.find_all('div', class_='col-md-3 mb-3 col-20 col-6')
-        for item in list_item:
-            draft_urls_list = []
-            draft_urls = item.find('div', class_='listItem').find('a').get("href")
-            draft_urls_list.append(draft_urls)
-            # print(draft_urls_list)# - checkpoint
-            for i in draft_urls_list:
-                characteristics1 = []
-                additional_photos = []
-                final_urls = f'https://allbazar.uz' + i
-                # print(final_urls) - #checkpoint
-                r = requests.get(final_urls).content
-                soup = bs(r, 'lxml')
-                main_img = soup.find('div', class_='col-10 position-relative').find('img').get('data-src')
-                main_photo = 'https://allbazar.uz' + main_img
-                dir = soup.find('div', class_="col-2").find_all('img')
-                for i in dir:
-                    imgs = short_url + i['data-big']
-                    additional_photos.append(imgs)
-                product_name = soup.find('h1', class_='h3 mb-3').get_text()
-                product_price = soup.find('span', class_='listPrice').get_text().replace('uzs',' сум')
-                # product_description = soup.find('div', class_='container mt-3').find('div', class_='mt-2')
-                characteristics = soup.find('div', attrs='br-block p-3 bg-white')
-                rows = characteristics.find_all(attrs='col-md-6')
+for i in product_ulr_list:
+    product_pictures = []
+    r = requests.get(i, headers=HEADERS)
+    soup = bs(r.content, 'lxml')
+    product_title = soup.find(class_='h3 mb-3').text
+    product_price = soup.find('span', class_='listPrice').get_text().replace('uzs', ' сум')
+    product_description = soup.find_all(class_="mt-2")[-1].get_text().strip().replace('Allbazar / ', '').replace(' ', ' ')
+    characteristics = soup.find('div', attrs='br-block p-3 bg-white')
+    product_cpecs = characteristics.find(class_='col-md-6').text.strip('\n').strip('\t')
+    dir = soup.find('div', class_="col-2").find_all('img')
+    for item in dir:
+        imgs = 'https://allbazar.uz' + item['data-big']
+        product_pictures.append(imgs)
 
-                for row in rows:
-                    for characteristic in row.find_all('div'):
-                        _ch = characteristic.text.strip('\n').strip('\t')
-                        characteristics1.append(_ch)
+    product_dict = {
+        'product_title' : product_title.replace('\n', '').replace('\t', ''),
+        'product_price' : product_price.replace('\n', '').replace('\t', ''),
+        'product_description' : product_description.replace('\n', '').replace('\t', ''),
+        'product_cpecs' : product_cpecs.replace('\n', '').replace('\t', ''),
+        'product_pics' : product_pictures
+    }
 
+    count += 1
+    print(f'#{count}: {i} is done...')
 
-                product['SKU'] = ('ALBZ')
-                product['category'] = ('Children')
-                product['sub_category'] = ('toys')
-                product['name'] = (product_name)
-                product['price'] = (product_price)
-                product['size'] = ('test')
-                product['color'] = ('test')
-                product['stock'] = ('1')
-                # product['description'] = (product_description)
-                product['characteristics1'] = (characteristics1)
-                product['main_photo'] = ( main_photo,)
-                product['additional_photos'] = (additional_photos)
-                akr = json.dumps(product, indent=4, ensure_ascii=False)
-                print(akr)
-                file.write(akr)
+    products.append(product_dict)
 
-parse_allbazar_toys()
+    with open('test.json', 'w', ) as json_file:
+        json.dump(products, json_file, indent=4, ensure_ascii=False)
 
 
-#починить продукт дескрипшн если на листе его нет дает ошибку функция try except должна сработать или функция If/else
-#добавить маржу
+
+
+
+# # добавить маржу
